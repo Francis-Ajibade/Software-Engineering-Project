978cadbbb17460bd927acea4a38ed60c83505983
ziond <francisoladimeji15@gmail.com>
2025-11-23 00:29:50 -0400

    updated files

--- Coordinator.java
+++ Coordinator.java
@@ -5,13 +5,13 @@ public class Coordinator {
 
     // === Collaborators managed internally ===
     private ListOfTestSuite listOfTestSuites;
-    private ListOfProgram listOfPrograms;
+    private ListOfPrograms listOfPrograms;
     private ListofTestCase testcaseList;
 
     // === Constructor ===
     public Coordinator() {
         this.listOfTestSuites = new ListOfTestSuite(); 
-        this.listOfPrograms = new ListOfProgram();
+        this.listOfPrograms = new ListOfPrograms();
         this.testcaseList = new ListofTestCase();
     }
 
